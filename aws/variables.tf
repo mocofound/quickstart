5f481aa43c5f15bf3bbc44005233cfaa9afcb0a7
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-10-14 12:53:23 -0500

    Update variables.tf

--- aws/variables.tf
+++ aws/variables.tf
@@ -5,3 +5,11 @@ variable "aws_owner" {
 variable "aws_TTL" {
   description = "TTL for maximum lifetime of deployed resources for reaper bot"
 }
+
+variable "vault_user" {
+  description = "name of HashiCorp Vault user"
+}
+
+variable "vault_addr" {
+  description = "address of HashiCorp Vault"
+}
